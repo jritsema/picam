947aebf0db0a32526c385efc9f580b76c6627847
John Ritsema <john_ritsema@yahoo.com>
2015-07-16 23:01:17 -0400

    implemented post /camera for picture taking

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 var express = require('express');
+var spawn = require('child_process').spawn;
+
 var app = express();
 
+var photoPath = __dirname + '/photos';
+
 app.post('/camera', function (req, res) {
-  //call:  raspistill -o ./photos/photo.jpg
+  var ls = spawn('raspistill', [ '-o', photoPath + '/photo.jpg', '-vf', '-hf', '-w', '1024', '-h', '768', '-n' ]);
+  ls.on('close', function (code) {
+    res.location('/photo.jpg');
+    res.end();
+  });
 });
 
-app.use(express.static(__dirname + '/photos'));
+app.use(express.static(photoPath));
 
 var server = app.listen(8000, function () {
   var host = server.address().address;
