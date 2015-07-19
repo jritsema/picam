f7c37bdfd9f329339b83dc27064c090850c6fcb3
John Ritsema <john_ritsema@yahoo.com>
2015-07-19 15:23:36 -0400

    added CORS support

--- index.js
+++ index.js
@@ -25,6 +25,13 @@ function takePicture(callback) {
 
 var app = express();
 
+//cors
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //api for taking a picture
 app.post('/camera', function (req, res) {
   takePicture(function (code) {
