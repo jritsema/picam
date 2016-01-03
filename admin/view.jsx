ff78fe4edd35ffe9c3f6f71f5154272fe4667c1b
John Ritsema <john_ritsema@yahoo.com>
2016-01-03 12:38:08 -0500

    ux improvements

--- admin/view.jsx
+++ admin/view.jsx
@@ -11,16 +11,20 @@ export default React.createClass({
 
   getInitialState() {
     return {
-      options: undefined,
+      loading: true,
       shooting: false,
+      options: undefined,
       imageUrl: this.baseUrl + 'photo.jpg'
     };
   },
 
   componentDidMount() {
-    request({ url: this.baseUrl + 'camera', json: true }, function(err, res, body) {
+    request({ url: this.baseUrl + 'camera', json: true }, (err, res, body) => {
       if (err) throw err;
-      this.setState({ options: body });
+      this.setState({
+        loading: false,
+        options: body
+      });
     }.bind(this));
   },
 
@@ -33,7 +37,7 @@ export default React.createClass({
     //tack on a unique value to the image url to bust the cache
     var uri = this.baseUrl + 'photo';
     var input = { cliArgs: this.state.options.cliArgs };
-    request({ method: 'POST', url: uri, json: input }, function (err, res) {
+    request({ method: 'POST', url: uri, json: input }, (err, res) => {
       if (err) { console.log('error posting photo'); alert(err); }
       this.setState({
         shooting: false,
@@ -43,31 +47,50 @@ export default React.createClass({
   },
 
   render() {
-    var content = (
+
+    //3 states:
+    //- loading (spinner only)
+    //- normal (everything)
+    //- shooting (replace photo with spinner)
+
+    let spinner = (
       <div>
         <br/>
         <CircularProgress mode="indeterminate" />
       </div>
     );
-    if (!this.state.shooting && this.state.options) {
-      content = (
-        <div>
-          <TextField
-            hintText="raspistill options"
-            floatingLabelText="raspistill options"
-            defaultValue={this.state.options.cliArgs}
-          />
-          <br/><br/>
-          <RaisedButton
-            label="Take Picture"
-            primary={true}
-            onMouseUp={this.onTakePicture}
-          />
-          <br/><br/>
-          <img src={this.state.imageUrl} />
-        </div>
-      );
-    }
+
+    let photo = <img src={this.state.imageUrl} />
+
+    let buttonText = this.state.shooting ? 'Taking Picture...' : 'Take Picture';
+
+    let options = this.state.options ? this.state.options.cliArgs : '';
+
+    let bottomPane = this.state.shooting ? spinner : photo;
+
+    let content = (
+      <div>
+        <TextField
+          hintText="raspistill options"
+          floatingLabelText="raspistill options"
+          defaultValue={options}
+        />
+        <br/><br/>
+        <RaisedButton
+          label={buttonText}
+          primary={true}
+          onMouseUp={this.onTakePicture}
+        />
+        <br/><br/>
+        {bottomPane}
+      </div>
+    );
+
+    //only show spinner if we're loading
+    if (this.state.loading)
+      content = spinner;
+
+    //render AppBar with content
     return (
       <div>
         <AppBar title={<span>picam</span>} />
